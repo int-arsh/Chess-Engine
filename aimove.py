c4e5e2b565639fadb809e895eec8dd19f666072b
Akash Jain <jainaakash303@gmail.com>
2024-08-18 17:14:10 +0530

    minmax for AI moves

--- aimove.py
+++ aimove.py
@@ -3,6 +3,7 @@ import random
 pieceScore = {"K": 0, "Q": 9, "R": 5, "B": 3, "N": 3, "p": 1}
 CHECKMATE = 1000
 STALEMATE = 0
+DEPTH = 3
 
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves)-1)]
@@ -39,6 +40,66 @@ def findBestMove(gs, validMoves):  # called for AI move
             bestPlayerMove = playerMove  # so we will have best black move in response of the best white move
         gs.undoMove()
     return bestPlayerMove
+
+
+'''
+Helper method to make first recursive call
+'''
+def findBestMoveMinMax(gs, validMoves):
+    global nextMove
+    nextMove = None  # if it doesn't find a move then it picks a random move
+    findMoveMinMax(gs, validMoves, DEPTH, gs.whiteToMove)
+    return nextMove
+
+def findMoveMinMax(gs, validMoves, depth, whiteToMove):
+    global nextMove
+    if depth == 0:
+        return scoreMaterial(gs.board)
+
+    if whiteToMove:
+        maxScore = -CHECKMATE
+        for move in validMoves:
+            gs.makeMove(move)
+            nextMoves = gs.getValidMoves()
+            score = findMoveMinMax(gs, nextMoves, depth - 1, False)
+            if score > maxScore:
+                maxScore = score
+                if depth == DEPTH:
+                    nextMove = move
+            gs.undoMove()
+        return maxScore
+
+    else:
+        minScore = CHECKMATE
+        for move in validMoves:
+            gs.makeMove(move)
+            nextMoves = gs.getValidMoves()
+            score = findMoveMinMax(gs, nextMoves, depth - 1, True)
+            if score < minScore:
+                minScore = score
+                if depth == DEPTH:
+                    nextMove = move
+            gs.undoMove()
+        return minScore
+
+def scoreBoard(gs):
+    if gs.checkMate:
+        if gs.whiteToMove:
+            return -CHECKMATE  # black wins
+        else:
+            return CHECKMATE
+    elif gs.staleMate:
+        return STALEMATE
+
+    score = 0
+    for row in gs.board:
+        for square in row:
+            if square[0] == 'w':
+                score += pieceScore[square[1]]
+            elif square[0] == 'b':
+                score -= pieceScore[square[1]]
+    return score
+
 
 
 '''
--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def main():
 
         # AI move finder
         if not gameOver and not humanTurn:
-            AIMove = aimove.findBestMove(gs, validMoves)
+            AIMove = aimove.findBestMoveMinMax(gs, validMoves)
             if AIMove is None:
                 AIMove = aimove.findRandomMove(validMoves)
             gs.makeMove(AIMove)
